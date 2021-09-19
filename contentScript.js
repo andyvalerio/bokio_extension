7abd1b5b9b57ef36aa265ac6cd73a6bb64b8e4df
andyvandermeyde <giuseppe@valerio.nu>
2021-09-19 14:39:21 +0200

    Make it work when updating existing product

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,11 @@
 const targetNode = document.getElementById('root');
 const config = { attributes: false, childList: true, subtree: true };
+const matchProductEdit = ['Redigera rad', 'Lägg till ny rad']
 
 const callback = function(mutationsList, observer) {
     for(const mutation of mutationsList) {
         if (mutation.addedNodes.length == 1 && mutation.addedNodes[0].innerText != undefined) {
-            if (mutation.addedNodes[0].innerText.startsWith('Lägg till ny rad') && mutation.addedNodes[0].className === 'Xp Wp') {
-                console.log('Adding product')
+            if (matchProductEdit.indexOf(mutation.addedNodes[0].innerText.substr(0, mutation.addedNodes[0].innerText.indexOf('\n'))) >= 0 && mutation.addedNodes[0].className === 'Xp Wp') {
                 labels = document.getElementsByTagName('label')
                 divToCopy = getFirstElementByText(labels, 'Totalt Exklusive VAT').parentElement
                 // Add a new node with the price including VAT
@@ -14,6 +14,7 @@ const callback = function(mutationsList, observer) {
                 newNode.childNodes[0].textContent = 'Totalt Inklusive VAT'
                 divToCopy.parentElement.appendChild(newNode)
                 vatSelector = divToCopy.parentElement.getElementsByTagName('select')[0]
+                updatePrice(vatSelector, divToCopy, newNode)
                 // Add listeners to the input textbox and the VAT selector
                 vatSelector.addEventListener('change', (event) => {
                     updatePrice(vatSelector, divToCopy, newNode)
@@ -41,9 +42,14 @@ function getFirstElementByText(elements, text) {
 }
 
 function updatePrice(vatSelector, priceDiv, newPriceDiv) {
-    beforePrice = Number(priceDiv.childNodes[1].textContent.replace(' kr', '').replace(',', '.'))
+    beforePrice = Number(replaceNbsps(priceDiv.childNodes[1].textContent).replace('kr', '').replace(',', '.'))
     vatPercentage = Number(vatSelector.selectedOptions[0].innerText.replace(' %', ''))
-    tax = vatPercentage * beforePrice / 100
+    tax = (vatPercentage * beforePrice) / 100
     afterPrice = beforePrice + tax
-    newPriceDiv.childNodes[1].textContent = afterPrice + ' kr'.replace('.', ',')
+    newPriceDiv.childNodes[1].textContent = (afterPrice + ' kr').replace('.', ',')
+}
+
+function replaceNbsps(str) {
+    var re = new RegExp(String.fromCharCode(160), 'g');
+    return str.replace(re, '');
 }
